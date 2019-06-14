67c12fd56709f1d7b7e7523001b518063faaf48e
gkarumba <gachegua@gmail.com>
2019-06-14 11:45:47 +0300

    Add the_save user  method

--- passwordLocker.py
+++ passwordLocker.py
@@ -9,3 +9,8 @@ class User:
         self.user = username
         self.psw = password
     
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
