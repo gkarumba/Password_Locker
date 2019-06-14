e3953cd86696223e55d5dd9802cdf167af974e84
gkarumba <gachegua@gmail.com>
2019-06-14 11:29:49 +0300

    Add the user list

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,4 +1,10 @@
 class User:
+    """
+    Class that generates new instance
+    """
+    user_list = [] #Empty user list
+    
     def __init__(self,username,password):
         self.user = username
         self.psw = password
+    
